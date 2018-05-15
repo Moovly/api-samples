4765325d0b89aa0cfd11883bec2e666abd3c92d4
Niels <nielsvermaut@gmail.com>
2018-05-15 19:03:06 +0200

    Add token call

--- automator-react/src/components/App.js
+++ automator-react/src/components/App.js
@@ -2,19 +2,47 @@ import React, { Component } from 'react';
 import logo from '../static/logo.svg';
 import '../styles/App.css';
 
-class App extends Component {
-  render() {
+class App extends Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      step: 0,
+      token: '',
+      isValid: false
+    }
+  }
+
+  handleValidateClick = () =>
+  {
+    fetch('https://api.moovly.com/user/v1/users/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.state.token}`,
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({isValid: true})
+      })
+      .catch(() => this.setState({isValid: false}))
+    ;
+  };
+
+  render()
+  {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>
+          <h2>Your token</h2>
+          <input type="text" onChange={(e) => this.setState({token: e.target.value})} value={this.state.token}/>
+          <button onClick={this.handleValidateClick}>Validate</button>
+          {this.state.isValid && <span>Your token is valid</span>}
+        </div>
       </div>
-    );
+    )
   }
 }
 
