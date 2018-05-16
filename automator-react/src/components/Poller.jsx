6e9b0d2a167e18380a99e80bd3a8e4a1a51e2c5c
Simon VDB <simonvdbroeck@gmail.com>
2018-05-16 20:54:26 +0200

    Poller refactor recommendation

--- automator-react/src/components/Poller.jsx
+++ automator-react/src/components/Poller.jsx
@@ -1,97 +1,97 @@
-import React from 'react';
+import * as React from "react";
 import validateResponse from "../util/validate-request";
 import moment from "moment";
 
-class Poller extends React.Component
-{
-  CHECK_INTERVAL = 60000;
-  interval = null;
+export default class Poller extends React.Component {
+  state = {
+    CHECK_INTERVAL: this.props.CHECK_INTERVAL || 60000,
+    intervalId: null,
+    lastTimeChecked: null,
+    jobStatus: null,
+    isFailed: false,
+    isFinished: false
+  };
 
-  constructor(props)
-  {
-    super(props);
+  handleGetJobStatus = () => {
+    this.setState({ lastTimeChecked: new Date().getTime() });
 
-    this.state = {
-      lastTimeChecked: null,
-      jobStatus: null,
-      isFailed: false,
-      isFinished: false,
-    }
-  }
-
-  handleGetJobStatus = () =>
-  {
-    this.setState({lastTimeChecked: (new Date()).getTime()});
-
-    fetch('https://api.moovly.com/generator/v1/jobs/' + this.props.jobId, {
-      method: 'GET',
+    fetch("https://api.moovly.com/generator/v1/jobs/" + this.props.jobId, {
+      method: "GET",
       headers: {
         Authorization: `Bearer ${this.props.token}`
       }
     })
       .then(validateResponse)
       .then(response => response.json())
-      .then(response => {this.setState({jobStatus: response}); return response})
-      .then(response => this.setState({isFinished: response.status === 'finished' || response.status === 'error'}))
-      .catch(() => this.setState({isFailed: true}))
+      .then(response => {
+        this.setState({
+          jobStatus: response,
+          isFinished: response.status === "finished" || response.status === "error"
+        });
+      })
+      .catch(() => this.setState({ isFailed: true }, this.clearInterval));
   };
 
-  componentDidMount()
-  {
+  componentDidMount() {
     this.handleGetJobStatus();
 
     // check every minute
-    this.interval = window.setInterval(this.handleGetJobStatus, this.CHECK_INTERVAL);
+    const intervalId = window.setInterval(this.handleGetJobStatus, this.state.CHECK_INTERVAL);
+    this.setState({ intervalId });
   }
 
-  componentDidUpdate(prevProps, prevState)
-  {
-    if (prevState.isFailed !== this.state.isFailed && this.state.isFailed) {
-      window.clearInterval(this.interval);
-    }
+  componentDidUpdate(prevProps, prevState) {
+    const { isFinished } = this.state;
 
-    if (prevState.isFinished !== this.state.isFinished && this.state.isFinished) {
-      window.clearInterval(this.interval);
+    if (prevState.isFinished !== isFinished && isFinished) {
+      this.clearInterval();
     }
   }
 
-  render()
-  {
+  clearInterval = () => {
+    window.clearInterval(this.state.intervalId);
+    this.setState({ intervalId: null });
+  };
+
+  render() {
     return (
-      <div className={`step step-poller ${this.props.isDone && 'step-done'}`}>
+      <div className={`step step-poller ${this.props.isDone && "step-done"}`}>
         <div className="step__info">
           <h2>Polling Job data</h2>
         </div>
         <div className="step__action">
-          {this.state.jobStatus !== null && <div className="job">
-            <div>Id: {this.props.jobId}</div>
-            <div>Status: {this.state.jobStatus.status}</div>
-            <div className="job__videos">
-              {this.state.jobStatus.videos.map(video => {
-                return (<div key={video.external_id}>
-                  <ul>
-                    <li>External Id: {video.external_id}</li>
-                    <li>Status: {video.status}</li>
-                    <li>
-                      {video.status === 'started' && <span>Result: TBD</span>}
-                      {video.status === 'success' && <a href={video.url}>Result: Video</a>}
-                      {video.status === 'failed' && <span>Result: Error - {video.error}</span>}
-                    </li>
-                  </ul>
-                </div>)
-              })}
-              {!this.state.isFinished && <div>
-                Last time checked: {moment(this.state.lastTimeChecked).format('h:mm:ss')},
-                Next check: {moment(this.state.lastTimeChecked).add(1, 'minutes').format('h:mm:ss')}
-              </div>}
+          {this.state.jobStatus !== null && (
+            <div className="job">
+              <div>Id: {this.props.jobId}</div>
+              <div>Status: {this.state.jobStatus.status}</div>
+              <div className="job__videos">
+                {this.state.jobStatus.videos.map(video => (
+                  <React.Fragment key={video.external_id}>
+                    <ul>
+                      <li>External Id: {video.external_id}</li>
+                      <li>Status: {video.status}</li>
+                      <li>
+                        {video.status === "started" && <span>Result: TBD</span>}
+                        {video.status === "success" && <a href={video.url}>Result: Video</a>}
+                        {video.status === "failed" && <span>Result: Error - {video.error}</span>}
+                      </li>
+                    </ul>
+                  </React.Fragment>
+                ))}
+                {!this.state.isFinished && (
+                  <div>
+                    Last time checked: {moment(this.state.lastTimeChecked).format("h:mm:ss")}, Next check:{" "}
+                    {moment(this.state.lastTimeChecked)
+                      .add(1, "minutes")
+                      .format("h:mm:ss")}
+                  </div>
+                )}
+              </div>
             </div>
-          </div>}
-
-          <button onClick={() => {window.location.reload()}}>Restart</button>
+          )}
+          <button onClick={window.location.reload}>Restart</button>
         </div>
       </div>
-    )
+    );
   }
 }
-
-export default Poller;
